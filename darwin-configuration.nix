425c7f39059b572528a4ab77965604244357d969
Victor Biga <victor.biga@gmail.com>
2024-09-18 08:14:37 +0000

    feat: init

---
+++ darwin-configuration.nix
@@ -0,0 +1,34 @@
+{ pkgs, userName, ... }: {
+
+  services.nix-daemon.enable = true;
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
+
+  #system.configurationRevision = self.rev or self.dirtyRev or null;
+
+  # Used for backwards compatibility. please read the changelog
+  # before changing: `darwin-rebuild changelog`.
+  system.stateVersion = 4;
+
+  # The platform the configuration will be used on.
+  # If you're on an Intel system, replace with "x86_64-darwin"
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  # Declare the user that will be running `nix-darwin`.
+  users.users.victor = {
+    name = userName;
+    home = "/Users/" + userName;
+  };
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs = {
+    zsh.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    nixpkgs-fmt
+    bat
+    git
+    neovim
+  ];
+}
---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, nix-darwin, nix-homebrew, ... }: let
+   userName = "victor";
+  in
+  {
+    darwinConfigurations."Victors-Virtual-Machine" = nix-darwin.lib.darwinSystem {
+      #specialArgs = { inherit overlays; };
+      system = "aarch64-darwin";
+      modules = [
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            # Install Homebrew under the default prefix
+            enable = true;
+
+            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+            enableRosetta = true;
+
+            # User owning the Homebrew prefix
+            user = userName;
+
+            # Automatically migrate existing Homebrew installations
+            autoMigrate = true;
+          };
+        }
+        ./darwin-configuration.nix
+      ];
+    };
+  };
+}
