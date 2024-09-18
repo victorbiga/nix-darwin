ebacef3916b0d8d5e3e019191a77b28e47798b51
Victor Biga <victor.biga@gmail.com>
2024-09-18 17:54:32 +0200

    feat: go

--- home.nix
+++ home.nix
@@ -15,5 +15,11 @@
   };
 
   # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+  programs = {
+    home-manager {
+      enable = true;
+    };
+    go {
+      enable = true;
+    };
 }
